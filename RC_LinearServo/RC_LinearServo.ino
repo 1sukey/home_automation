85f9a235736e5f1556a3ce4e5eac2b87bbee056a
dzzie <dzzie@yahoo.com>
2017-02-12 10:00:50 -0500

    linear servo cleanup

--- RC_LinearServo/RC_LinearServo.ino
+++ RC_LinearServo/RC_LinearServo.ino
@@ -4,6 +4,15 @@
     Example code to add a servo interface to a standard linear actuator
     using a linear pot, L298N driver board, Arduino, and a typical RC Controller
     
+    two things to verify before you run this..
+      1) that stick up makes the actuator travel out
+      2) that the pot increases in resistance (higher number) as it travels out
+    
+    these can be easily reversed if your wires are flipped. you will destroy your pot 
+    if the readings are backwards.
+    
+    materials:
+    ---------------------------------------------------------------------------------------
     driver board: $7
         https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=L298N
     
@@ -45,70 +54,53 @@ void motorOff(){
     analogWrite(enable1, 0);
 }
 
-/*
-    so the rc stick will give us the following: (center range is for dead stick, 0 is controller off)
-        rc   |  min  |  max
-        -------------------
-        up   | 1530  | 2000
-        down | 1460  | 1000
-
-    our linear pot will give us readings from 0-1023, since we travel both ways, center pot must be
-    center travel. note that we dont go all the way to 0 or 1023 because we could damage the pot with 340lbs of force..
-         pot  |  min  |  max
-        -------------------
-        up   | 513   | 1000
-        down | 20    | 512
-  
-
-*/
-
 //if we overshoot we just shutoff, we dont try to hone in by reversing 
 //direction until perfect..oscillations are worse this isnt exact..
 void moveLinearActuatorUntil(int targetPotPos, int rawRCPos){
     
     char buf[255];
-    int curPotPos, v1, v2, speed = 255; //full speed 
-    int delta = 0;
-    bool moveIn;
+    int speed = 255; //full speed 
 
-    curPotPos = analogRead(linearPot);
+    if(rawRCPos == 0){
+       motorOff();  
+       Serial.println(" no signal");   
+       return;
+    }
+  
+    int curPotPos = analogRead(linearPot);
 
-    if( abs(targetPotPos - curPotPos) <= 40 ){
+    if( abs(targetPotPos - curPotPos) <= 25 ){  //any less and we can get seeking
          Serial.println("position obtained!"); 
          motorOff();  
          return;
     }
 
-    moveIn =  curPotPos > targetPotPos ? true : false;
-    v1 = moveIn ? LOW : HIGH;
-    v2 = moveIn ? HIGH : LOW;
+    bool moveIn =  curPotPos > targetPotPos ? true : false;
+    int v1 = moveIn ? LOW : HIGH;
+    int v2 = moveIn ? HIGH : LOW;
 
     while(1){
 
         int rc3 = pulseIn(rc_channel3, HIGH, 25000);
         curPotPos = analogRead(linearPot);    
         
-        //if we lost connection to controller or if user moved joystick > 30 then abort this seek of position..
-        if(rc3 == 0 || abs(rawRCPos - rc3) > 30){
+        //if we lost connection to controller or if user moved joystick then abort this seek of position..
+        if(rc3 == 0 || abs(rawRCPos - rc3) > 10){ //how sensitive do you want it to changes in stick position?
             motorOff();
             break;
         }
                 
         if(moveIn){
-            if( curPotPos <= targetPotPos){
-                motorOff();
-                break;
-            }
+            if( curPotPos <= targetPotPos){ motorOff(); break; }
         }
         else{
-            if( curPotPos >= targetPotPos){
-                motorOff();
-                break;
-            }
+            if( curPotPos >= targetPotPos){ motorOff(); break; }
         }
 
-        //speed of 50 is to small to move motor just makes it buzz..disabled for now..
-        //if( abs(curPotPos - targetPotPos) < 30 ) speed = 125; //drop to 1/4 speed for last bit
+        //try to slow speed for last bit to increase precision without overshoot/seeking
+        //if speed is to small motor will just buzz and not move :-\
+        //150 has a buzz..not sure if this extra complexity really helps precision...
+        if( abs(curPotPos - targetPotPos) < 25 ) speed = 180; 
 
         sprintf(buf, "curPos:%d  targetPos: %d  speed:%d   %s", curPotPos, targetPotPos, speed, moveIn ? "moving IN" : "moving OUT"); 
         Serial.println(buf);
@@ -118,35 +110,21 @@ void moveLinearActuatorUntil(int targetPotPos, int rawRCPos){
         analogWrite(enable1, speed);
     }
 
+    motorOff(); 
 
 }
 
 void loop() {
   
-  char buf[255];
+  char buf[255]; 
   int curPotPos = analogRead(linearPot);
   int rc3 = pulseIn(rc_channel3, HIGH, 25000);
-  int targetPotPos;
-
-  sprintf(buf, "rc3:%d  curPos:%d  ", rc3, curPotPos); 
-  Serial.print(buf);
+  int targetPotPos = map(rc3, 1000, 2000, 20, 1000); //map rc stick position to target slide pot value
   
-  if(rc3==0){
-     motorOff();  
-     Serial.println(" no signal");   
-  }
-  else if(rc3 > 1500){ //stick up
-      targetPotPos = map(rc3, 1530, 2000, 513, 1000); //map rc up position to where our slide pot needs to be
-      sprintf(buf, "stick up: curPos:%d  targetPos: %d ", curPotPos, targetPotPos); 
-      Serial.print(buf);
-      moveLinearActuatorUntil(targetPotPos, rc3);
-  }
-  else{
-      targetPotPos = map(rc3, 1460, 1000, 512, 20); //map rc up position to where our slide pot needs to be
-      sprintf(buf, "stick down: curPos:%d  targetPos: %d ", curPotPos, targetPotPos); 
-      Serial.print(buf);
-      moveLinearActuatorUntil(targetPotPos, rc3);
-  }
-    
+  sprintf(buf, "rc3:%d  curPos:%d  targetPos: %d  stick: %s", rc3, curPotPos, targetPotPos, rc3 > 1500 ? "UP" : "DOWN"); 
+  Serial.print(buf);
+
+  moveLinearActuatorUntil(targetPotPos, rc3);
+ 
   delay(10);
 }
